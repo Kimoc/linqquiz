542ae3d72d199b2e5f3da471cf100ca990ca76bf
unknown <vesprada@A005PC10.CENTRO>
2019-10-18 19:59:49 +0200

    comit

--- Programa/Program.cs
+++ Programa/Program.cs
@@ -1,30 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using Modelo;
-
+using System.Linq;
 namespace Programa
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<Equipo> equipos = LigaDAO.Instance.Equipos;
+            List<Jugador> jugadores = LigaDAO.Instance.Jugadores;
+            List<Partido> partidos = LigaDAO.Instance.Partidos;
+ 
             Console.WriteLine("Equipos :");
-            foreach (var e in LigaDAO.Instance.Equipos)
-            {
-                Console.WriteLine("\t" + e);
-            }
-
-            Console.WriteLine("Jugadores:");
-            foreach (var j in LigaDAO.Instance.Jugadores)
-            {
-                Console.WriteLine("\t" + j);
-            }
-
-            Console.WriteLine("Partidos:");
-            foreach (var p in LigaDAO.Instance.Partidos)
-            {
-                Console.WriteLine("\t" + p);
-            }
-
+            equipos.ForEach(Console.WriteLine);
+            Console.WriteLine("Jugadores :");
+            jugadores.ForEach(Console.WriteLine);
+            Console.WriteLine("Partidos :");
+            partidos.ForEach(Console.WriteLine);
             Console.ReadLine();
 
         }
